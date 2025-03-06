377aa40129f7dfb275cd58b04643878767567c60
StaticSnap <tylerrcox@outlook.com>
2025-03-05 22:19:04 -0800

    added backend folder

---
+++ GroupProject/Back End/CentralBackend.cs
@@ -0,0 +1,9 @@
+﻿namespace GroupProject
+{
+    /// <summary>
+    /// Primary backend class for implementing central logic for entire cite.
+    /// </summary>
+    public class CentralBackend
+    {
+    }
+}
